9f515544e866c2b75b9768838f8859c734e93812
Alex Vizhalil <avizha2@uic.edu>
2017-08-31 17:28:02 -0500

    moved method around and added a small algorithm to find a sub-grid

--- src/Sudoku/sudoku.java
+++ src/Sudoku/sudoku.java
@@ -19,45 +19,10 @@ public class sudoku {
 	}
 } 
 
-class board {
-	private int rows;
-	private int cols;
-	private int randStart;
-	
-	public board()
-	{
-		rows = 9;
-		cols = 9;
-		
-	}
-	void draw () {
-		//draw 9*9 board
-	}
-	
-	void populate (solution sol) {
-		//randomly select 6 numbers in each box to populate board with
-	}
-}
-
-class solution{
-	
-	int numbers() {
-		//generating random number 1-9
-	    Random rand = new Random();
-	    int randnum = rand.nextInt((9 - 1) + 1) + 1;
-	    return randnum;
-	}
-	//generate random solution
-	/* commented out to get rid of errors
-	void check(location loc, response res) {
-		//checks answer to solution
-	}*/
-	
-}
-
 class environment {
 	//generate environment
 	//includes background, buttons
+	//load GUI frames
 }
 
 
